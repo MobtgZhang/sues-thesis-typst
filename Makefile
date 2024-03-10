d9919eed780bab35ccc47f397a6dea13e27c4f3e
mobtgzhang <mobtgzhang@outlook.com>
2024-03-10 10:39:49 +0800

    删除了一些文件

--- Makefile
+++
@@ -1,7 +0,0 @@
-TYPST_FLAGS += --font-path fonts
-
-thesis:
-	typst compile ${TYPST_FLAGS} thesis.typ
-
-watch:
-	typst watch ${TYPST_FLAGS} thesis.typ
--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-chcp 65001>nul
-set Path=%cd%\;%Path%
-set TYPST_FONT_PATHS=%cd%\fonts
-echo 编译中……
-typst compile --font-path fonts thesis.typ
-echo 命令运行完毕，按任意键退出
-pause>nul
